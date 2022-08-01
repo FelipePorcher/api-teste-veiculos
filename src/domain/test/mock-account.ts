ff5029901a533b86b5701e7f29e1c062bbbb24ea
Felipe Porcher <felipeb0607@hotmail.com>
2022-08-01 02:34:56 -0300

    test: adiciona mocks(simuladores) de modelos e casos de uso da camada de dominio

---
+++ src/domain/test/mock-account.ts
@@ -0,0 +1,24 @@
+import { AccountModel } from '@/domain/models/account'
+import { AddAccountParams } from '@/domain/usecases/account/add-account'
+import { AuthenticationParams } from '@/domain/usecases/account/authentication'
+import { faker } from '@faker-js/faker'
+
+export const mockAddAccountParams = (): AddAccountParams => ({
+  name: faker.name.findName(),
+  cpf: faker.datatype.number({ max: 99999999999 }),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  id: faker.database.mongodbObjectId(),
+  name: faker.name.findName(),
+  cpf: faker.datatype.number({ max: 99999999999 }),
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
+
+export const mockAuthenticationParams = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
