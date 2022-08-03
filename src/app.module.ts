25fb361694e6bf968f9c0b84084c4646e2a00ba8
Felipe Porcher <felipeb0607@hotmail.com>
2022-08-02 23:54:01 -0300

    feat: adiciona url do banco de dados do mongo (evitado usar dotenv)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
+import { SignupModule } from './presentation/controllers/signup/signup.module'
+import { MongooseModule } from '@nestjs/mongoose'
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://admin:zTz3ELmuG1IfVbnn@cluster0.exnwf.mongodb.net/teste-veiculos?retryWrites=true&w=majority'),
+    SignupModule],
   controllers: [],
   providers: []
 })
