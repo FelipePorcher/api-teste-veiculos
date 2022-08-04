0b81ee40e7295abc3dc038b13ea6052d6c45d7f0
Felipe Porcher <felipeb0607@hotmail.com>
2022-08-04 04:30:10 -0300

    feat: adiciona swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import 'module-alias/register'
 import { AppModule } from './app.module'
 
 async function bootstrap (): Promise<void> {
   const app = await NestFactory.create(AppModule)
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Teste Veiculos API')
+    .setVersion('0.0.1')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, configSwagger)
+  SwaggerModule.setup('doc', app, document)
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000)
 }
