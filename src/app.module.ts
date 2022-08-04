cc0c544696f18d59732be191d2eec2ff2504db59
Felipe Porcher <felipeb0607@hotmail.com>
2022-08-04 04:30:48 -0300

    feat: adiciona modulos na api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common'
 import { SignupModule } from './presentation/controllers/signup/signup.module'
 import { MongooseModule } from '@nestjs/mongoose'
+import { LoginModule } from './presentation/controllers/login/login.module'
+import { VehicleModule } from './presentation/controllers/vehicle/vehicle.module'
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb+srv://admin:zTz3ELmuG1IfVbnn@cluster0.exnwf.mongodb.net/teste-veiculos?retryWrites=true&w=majority'),
-    SignupModule],
+    SignupModule,
+    LoginModule,
+    VehicleModule],
   controllers: [],
   providers: []
 })
